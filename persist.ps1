ffd121f99c3bbbf29784920b0a8ac9aa6aa44ff7
mathild <mathild@users.noreply.github.com>
2014-11-11 12:23:24 +0100

    Delete persist.ps1

--- persist.ps1
+++
@@ -1,4 +0,0 @@
-$wd = new-object -comobject word.application
-$wd.documents.open("$env:userprofile\AppData\Roaming\Microsoft\Templates\Normal.dotm")
-$wd.run("AutoExec")
-$wd.quit() # exit application
